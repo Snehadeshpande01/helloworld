808f9a3b61c6ece6ac608f10bd4874da01f1faa1
Snehadeshpande01 <98334365+Snehadeshpande01@users.noreply.github.com>
2022-05-30 23:59:12 +0530

    Create employee.java

---
+++ employee.java
@@ -0,0 +1,21 @@
+import java.util.*;
+class employee
+
+{
+
+public static void main (String args[])
+
+{
+
+System.out.println("Name Year of joining Address");
+
+System.out.println("Robert 1994 64C- WallsStreat");
+
+System.out.println("ritu 2000 688- WallsStreat");
+
+System.out.println("Johnny 1999 27B- WallsStreat");
+
+}
+
+}
+
